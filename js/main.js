a935adc7aaeede4f2700df9f56dab0aa02a2b5c2
issam-errami <devissamerrami@gmail.com>
2022-11-02 18:34:48 +0100

    commit task

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(function ($) {
+   
+
+    // Team carousel
+    $(".team-carousel, .related-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 45,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:1
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+    });
+    
+})(jQuery);
+
